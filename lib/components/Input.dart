fac367ea815ea1b2b49bc7d2e5e29a05873184dc
Isaque Precioso <caracktergames@gmail.com>
2025-10-19 18:44:39 -0300

    feat: componente de input padrao

--- lib/components/Input.dart
+++ lib/components/Input.dart
@@ -5,22 +5,32 @@ class Input extends StatelessWidget {
   final TextEditingController controller;
   final TextInputType keyboardType;
   final int maxLines;
+  final String placeholder;
 
   const Input({
     super.key,
     required this.label,
     required this.controller,
+    this.placeholder = '',
     this.keyboardType = TextInputType.text,
     this.maxLines = 1,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: const InputDecoration(
-        labelText: 'Nome',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: controller,
+        keyboardType: keyboardType,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          labelText: label,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder.isEmpty ? 'Digite seu $label' : placeholder,
+          border: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12.0)),
+          ),
         ),
       ),
     );
