1e08779d1baf9e57ce90625d3240fd1a3d03b78a
Isaque Precioso <caracktergames@gmail.com>
2025-10-19 19:22:50 -0300

    feat: tela Cadastro de Clientes

--- lib/screens/newclient_screen.dart
+++ lib/screens/newclient_screen.dart
@@ -1,4 +1,8 @@
+import 'package:app_paula_barros/components/button.dart';
+import 'package:app_paula_barros/components/input_date.dart';
+import 'package:app_paula_barros/components/input_textarea.dart';
 import 'package:flutter/material.dart';
+import 'package:app_paula_barros/components/input.dart';
 
 class NewClientScreen extends StatelessWidget {
   const NewClientScreen({super.key});
@@ -8,42 +12,78 @@ class NewClientScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Novo Cliente'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
+      appBar: AppBar(title: const Text('Novo Cliente')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Nome',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12.0)),
-              ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // TODO: abrir seletor de imagem
+                          },
+                          child: CircleAvatar(
+                            radius: 120, // maior tamanho
+                            backgroundColor: Colors.grey[300],
+                            child: const Icon(
+                              Icons.person,
+                              size: 200, // ajusta o ícone ao novo raio
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {
+                            // TODO: abrir seletor de imagem
+                          },
+                          child: const Text('Adicionar foto'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Input(
+                  label: 'Nome',
+                  controller: TextEditingController(),
+                  placeholder: 'Digite o nome da cliente',
+                ),
+                Input(
+                  label: 'Telefone',
+                  controller: TextEditingController(),
+                  keyboardType: TextInputType.phone,
+                  placeholder: '(11) 99999-9999',
+                ),
+                InputDate(
+                  label: 'Data de Nascimento',
+                  controller: TextEditingController(),
+                  keyboardType: TextInputType.phone,
+                ),
+                Input(
+                  label: 'Email',
+                  controller: TextEditingController(),
+                  keyboardType: TextInputType.emailAddress,
+                  placeholder: 'email@exemplo.com',
+                ),
+                Textarea(
+                  label: 'Anotações',
+                  controller: TextEditingController(),
+                  maxLines: 6,
+                ),
+                const SizedBox(height: 20),
+                Button(label: 'Salvar Cliente', onPressed: () {}),
+              ],
             ),
-          ),
-          const SizedBox(height: 16),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Telefone',
-              border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.phone,
-          ),
-          const SizedBox(height: 16),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Notas',
-              border: OutlineInputBorder(),
-            ),
-            maxLines: 4,
-          ),
-          const SizedBox(height: 24),
-          ElevatedButton(
-            onPressed: () {
-              // Lógica para salvar o novo cliente
-            },
-            child: const Text('Salvar Cliente'),
           ),
         ],
       ),
